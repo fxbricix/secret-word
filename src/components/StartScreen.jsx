41c250c52e1730cf2a08d882e37b8b79cd1a0825
Fabricio Moreira <fabriiciobr@outlook.com>
2024-07-11 23:30:35 -0300

    improving start screen

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,11 +1,13 @@
-import "./StartScreen.css";
+import style from "./StartScreen.css";
 
 const StartScreen = ({ buttonStartGame }) => {
   return (
     <div className="start">
       <h1>Secret Word</h1>
-      <p>Clique no botão abaixo para começar a jogar!</p>
+      <h3>Clique no botão abaixo para começar a jogar!</h3>
       <button onClick={buttonStartGame}>Começar o jogo</button>
+      <p className="info">Jogo feito por Fabricio Moreira</p>
+      <p className="info">Atendendo a um projeto do curso de REACT da Udemy</p>
     </div>
   );
 };
